18a4eae873c2a27175fbfa5ecc6ae296d8a79aba
omprakashchavan01 <askomdch@gmail.com>
2025-01-06 15:44:48 +0530

    changes

---
+++ default.config.js
@@ -0,0 +1,37 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 10,
+  reporter: [
+    ['html'],
+    ['list']
+  ],
+  use: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1920, height: 1080 },
+        launchOptions: {
+          args: ['--disable-dev-shm-usage']
+        }
+      },
+    }
+  ],
+  outputDir: 'test-results/',
+});
